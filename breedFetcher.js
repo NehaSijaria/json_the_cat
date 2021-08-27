78b7503ed4c8c04d1235c3bbbeb73b335f65ac0d
NehaSijaria <nehasijaria@gmail.com>
2021-08-27 17:54:10 -0400

    JSON the cat refatoring

---
+++ breedFetcher.js
@@ -0,0 +1,20 @@
+  const request = require('request');
+
+const fetchBreedDescription = function (breedName, callback){
+const name = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+request(name, (error, response, body) => {
+  if (error) {
+    callback(error, null);
+  }
+  const data = JSON.parse(body);
+  if (data.length === 0) {
+    errMessage = `The breed ${breedName} is not found`;
+    callback(errMessage, null);
+  } else {
+    callback(null, data[0].description);
+  }
+});
+};
+
+module.exports = { fetchBreedDescription };
+
